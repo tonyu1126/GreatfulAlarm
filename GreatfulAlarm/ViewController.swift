4c9f61c04fd73416f6f552616e0c21e3041e31c2
YuTaNim <yuta.tanimura425@gmail.com>
2015-11-07 20:46:24 +0900

    Initial Commit

---
+++ GreatfulAlarm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GreatfulAlarm
+//
+//  Created by 谷村佑太 on 2015/11/07.
+//  Copyright © 2015年 Yuta Tanimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GreatfulAlarmUITests/GreatfulAlarmUITests.swift
@@ -0,0 +1,36 @@
+//
+//  GreatfulAlarmUITests.swift
+//  GreatfulAlarmUITests
+//
+//  Created by 谷村佑太 on 2015/11/07.
+//  Copyright © 2015年 Yuta Tanimura. All rights reserved.
+//
+
+import XCTest
+
+class GreatfulAlarmUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
